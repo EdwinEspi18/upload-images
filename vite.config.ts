fad1e5da4f145c5b3e77b036cdaf414644228bb0
Edwin A. Espinal Taveras <espinaledwin1919@gmail.com>
2024-02-05 16:54:32 -0400

    fix: :bug: Skip Typescript
    strict in false

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [checker({typescript: false}), react()],
+});
